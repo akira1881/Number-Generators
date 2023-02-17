21b9cc12aa3ec2ef1375196e297d5b1116768ffc
Charina Vallecera <charinavallecera08@gmail.com>
2023-02-17 21:38:17 +0800

    1st commit - def main

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# Programmed by: Charina C. Vallecera
+# BSCOE 2-2
+# DSA Final Project
+# Number Generators
+
+def main():
+    print("\n <<<<<< MAIN >>>>>> \n")
+    print("1. Sequence Generator ")
+    print("2. Sorting Numbers")
+    print("3. Generating Random Numbers")
+    print("4. Text to Number Generator")
+    print("5. Exit the Program")
